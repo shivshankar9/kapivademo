0ab5376c094ca71793fa744c8b73138a378f0dfe
shivshankar9 <shivshankarkumar281@gmail.com>
2025-04-02 00:11:36 +0530

    Update index.tsx

--- src/components/layout/Navbar/TopNavbar/index.tsx
+++ src/components/layout/Navbar/TopNavbar/index.tsx
@@ -91,7 +91,7 @@ const TopNavbar = () => {
               "text-2xl lg:text-[32px] mb-2 mr-3 lg:mr-10",
             ])}
           >
-            Kapiva.com
+           THANDRA AYURUSHA
           </Link>
         </div>
         <NavigationMenu className="hidden md:flex mr-2 lg:mr-7">
